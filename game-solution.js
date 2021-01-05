197efee460588380b5a1f696b7541bffcbab5165
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-01-04 19:15:57 -0500

    trying to figure out the gaming problem

--- game-solution.js
+++ game-solution.js
@@ -1,3 +1,40 @@
+// const commands = require("./game-store");
+
+// const setUp = (command) => {
+//   let commandArray = command.split(" ");
+//   return { [commandArray[0]]: Number(commandArray[1]), touched: false };
+// };
+
+// const accCount = () => {
+//   const commandArray = commands.split(/\n/).map(setUp);
+//   return executor(0, commandArray);
+// };
+
+// const executor = (index, commandArray) => {
+//   let acc = 0;
+//   const commandObj = commandArray[index];
+//   if (commandObj.touched === true) return acc;
+//   commandObj.touched = true;
+//   const command = Object.keys(commandObj)[0];
+//   if (command === "nop") {
+//     acc += executor(index + 1, commandArray);
+//   }
+//   if (command === "jmp") {
+//     acc += executor(index + commandObj[command], commandArray);
+//   }
+//   if (command === "acc") {
+//     acc += commandObj[command] + executor(index + 1, commandArray);
+//   }
+//   return acc;
+// };
+
+// console.log(accCount())
+
+
+/* 
+------------------------------------------------PART TWO OF THE PROBLEM ----------------------------------------------------
+*/
+
 const commands = require("./game-store");
 
 const setUp = (command) => {
@@ -7,25 +44,49 @@ const setUp = (command) => {
 
 const accCount = () => {
   const commandArray = commands.split(/\n/).map(setUp);
-  return executor(0, commandArray);
+  return findProblem(0, commandArray);
 };
 
-const executor = (index, commandArray) => {
-  let acc = 0;
+const switchOp = (command) => {
+    if (Object.keys(command)[0] === "jpm"){
+        return {"nop": command["jmp"], touched: true};
+    }
+    if (Object.keys(command[0]) === "nop"){
+        return {"jmp": command["nop"], touched: true};
+    }
+    return command;
+}
+
+const findProblem = (index, commandArray, indexToSwitch) => {
+  let problem = -1;
+  if (indexToSwitch === index) {
+      commandArray[index] = switchOp(commandArray[index])
+  }
   const commandObj = commandArray[index];
-  if (commandObj.touched === true) return acc;
   commandObj.touched = true;
   const command = Object.keys(commandObj)[0];
   if (command === "nop") {
-    acc += executor(index + 1, commandArray);
+    if (commandArray[index + 1].touched){
+        return index
+   }
+    problem = findProblem(index + 1, commandArray);
   }
   if (command === "jmp") {
-    acc += executor(index + commandObj[command], commandArray);
+    if (commandArray[index + commandObj[command]].touched){
+        return index 
+    }
+    problem = findProblem(index + commandObj[command], commandArray);
   }
   if (command === "acc") {
-    acc += commandObj[command] + executor(index + 1, commandArray);
+    if (commandArray[index + 1].touched){
+         return index 
+    }
+    problem = findProblem(index + 1, commandArray);
   }
-  return acc;
+  return problem;
 };
 
 console.log(accCount())
+
+//you should fix the logic here!!!!!
+
--- game-store.js
+++ game-store.js
@@ -654,4 +654,15 @@ acc +1
 jmp -4
 acc +6`
 
+const commandsShort2 = 
+`nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
 module.exports = commands;
